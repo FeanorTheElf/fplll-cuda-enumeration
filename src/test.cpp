c96066653b1f887dddac78f8c879974f3203940f
Simon Pohmann <simon@pohmann.de>
2020-12-02 11:00:24 +0100

    Cleaned up tests

--- src/test.cpp
+++ src/test.cpp
@@ -25,31 +25,71 @@ float find_initial_radius(const float* lattice, const unsigned int n) {
     return sqrt(result);
 }
 
-void test_fplll_like() {
-  
-  constexpr unsigned int total_dim       = 50;
-  const std::array<std::array<float, total_dim>, total_dim> &lattice = test_mu_knapsack_big;
-
-  double maxdist = find_initial_radius(&lattice[0][0], total_dim) * 1.05;
-  std::cout << maxdist << std::endl;
-  maxdist = maxdist * maxdist; 
-  std::function<extenum_cb_set_config> set_config = [&lattice](double *mu, size_t mudim, bool mutranspose, double *rdiag, double *pruning) {
+void set_lattice_config(const float* lattice, double* mu, size_t mudim, bool mutranspose, double* rdiag, double* pruning) {
     assert(mutranspose);
     for (unsigned int i = 0; i < mudim; ++i) {
-      for (unsigned int j = i; j < mudim; ++j) {
-        mu[i * mudim + j] = lattice[i][j] / lattice[i][i];
-      }
-      rdiag[i] = lattice[i][i] * lattice[i][i];
+        for (unsigned int j = i; j < mudim; ++j) {
+            mu[i * mudim + j] = lattice[i * mudim + j] / lattice[i * mudim + i];
+        }
+        rdiag[i] = lattice[i * mudim + i] * lattice[i * mudim + i];
     }
+}
+
+void test_small() {
+  
+  constexpr unsigned int total_dim       = 20;
+  const std::array<std::array<float, total_dim>, total_dim> &lattice = test_mu_small;
+
+  double maxdist = find_initial_radius(&lattice[0][0], total_dim) * 1.05;
+  maxdist = maxdist * maxdist; 
+  std::function<extenum_cb_set_config> set_config = [&lattice](double *mu, size_t mudim, bool mutranspose, double *rdiag, double *pruning) {
+      set_lattice_config(&lattice[0][0], mu, mudim, mutranspose, rdiag, pruning);
   };
   std::function<extenum_cb_process_sol> process_sol = [](double norm_square, double* x)-> double { return norm_square; };
   fplll_cuda_enum(total_dim, maxdist, set_config, process_sol, nullptr, false, false);
 }
 
+void test_knapsack() {
+
+    constexpr unsigned int total_dim = 40;
+    const std::array<std::array<float, total_dim>, total_dim>& lattice = test_mu_knapsack_normal;
+
+    double maxdist = find_initial_radius(&lattice[0][0], total_dim) * 1.05;
+    maxdist = maxdist * maxdist;
+    std::function<extenum_cb_set_config> set_config = [&lattice](double* mu, size_t mudim, bool mutranspose, double* rdiag, double* pruning) {
+        set_lattice_config(&lattice[0][0], mu, mudim, mutranspose, rdiag, pruning);
+    };
+    std::function<extenum_cb_process_sol> process_sol = [](double norm_square, double* x)-> double { return norm_square; };
+    fplll_cuda_enum(total_dim, maxdist, set_config, process_sol, nullptr, false, false);
+}
+
+void test_perf() {
+
+    constexpr unsigned int total_dim = 50;
+    const std::array<std::array<float, total_dim>, total_dim>& lattice = test_mu_knapsack_compare;
+
+    double maxdist = find_initial_radius(&lattice[0][0], total_dim) * 1.05;
+    maxdist = maxdist * maxdist;
+    std::function<extenum_cb_set_config> set_config = [&lattice](double* mu, size_t mudim, bool mutranspose, double* rdiag, double* pruning) {
+        set_lattice_config(&lattice[0][0], mu, mudim, mutranspose, rdiag, pruning);
+    };
+    std::function<extenum_cb_process_sol> process_sol = [](double norm_square, double* x)-> double { return norm_square; };
+    fplll_cuda_enum(total_dim, maxdist, set_config, process_sol, nullptr, false, false);
+}
+
 int main()
 {
   try {
-    test_fplll_like();
+      test_small();
+      std::cout << "test_small() successful!" << std::endl << std::endl;
+      test_knapsack();
+      std::cout << "test_knapsack() successful!" << std::endl << std::endl;
+#ifdef PERF_TEST
+      test_perf();
+      std::cout << "test_perf() successful!" << std::endl << std::endl;
+#endif
+      std::cout << "=============" << std::endl;
+      std::cout << "All test successfull!" << std::endl;
   }
   catch (const char* ex) {
       std::cerr << "Failed: " << ex << std::endl;
